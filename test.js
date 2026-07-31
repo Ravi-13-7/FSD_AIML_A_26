b361e4b7ba23a859ebddb84e8ddafcba786a335f
Ravi-13-7 <ravisinghnoida2007@gmail.com>
2026-07-31 13:35:27 +0530

    import & export

---
+++ test.js
@@ -0,0 +1,9 @@
+import {sum, add} from "./demo5.js";
+
+console.log("Sum = ", sum(1));
+console.log("Sum = ", sum(1, 32, 44, 53, 32));
+console.log("Sum = ", sum(10, 20, 30));
+
+console.log("Add = ", add(1));
+console.log("Add = ", add(1, 32, 44, 53, 32));
+console.log("Add = ", add(10, 20, 30));
